6dcaa5f08aa8dc725ceaad3b5c91a9424520e22a
Stephen-Hao <35420905+Stephen-Hao@users.noreply.github.com>
2018-10-17 12:51:05 +0800

    Update ai-competition.js

--- src/js/ai-competition.js
+++ src/js/ai-competition.js
@@ -1,6 +1,6 @@
 $(function() {
     $('#content_container').fullpage({
-        sectionsColor: ['#fff', '#fff', '#fff', '#fff', '#f7f7f7', '#fff', '#fff', '#F5F7F9'],
+        sectionsColor: ['#fff', '#fff', '#fff', '#f7f7f7', '#fff', '#fff', '#F5F7F9'],
         anchors: ['index', 'introduction', 'fashion-style', 'prize', 'comp-date', 'organizer', 'contanct-us'],
         menu: '#menu'
     });

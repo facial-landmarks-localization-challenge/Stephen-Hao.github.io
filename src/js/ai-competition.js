ae93a8b6989373b418afbe0e78ec2cd55cabcd21
facial-landmarks-localization-challenge <44342608+facial-landmarks-localization-challenge@users.noreply.github.com>
2018-12-07 18:19:31 +0800

    Update ai-competition.js

--- src/js/ai-competition.js
+++ src/js/ai-competition.js
@@ -1,8 +1,7 @@
-/*$(function() {
+$(function() {
     $('#content_container').fullpage({
         sectionsColor: ['#fff', '#fff', '#fff', '#f7f7f7', '#fff', '#fff', '#F5F7F9'],
         anchors: ['index', 'introduction', 'fashion-style', 'prize', 'comp-date', 'organizer', 'contanct-us'],
         menu: '#menu'
     });
 });
-*/
